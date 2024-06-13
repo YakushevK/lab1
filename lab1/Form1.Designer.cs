b0c80ed454f9124f389e2c671b7a2784be7916c5
YakushevK <67880453+YakushevK@users.noreply.github.com>
2024-06-13 19:09:09 +0300

    удаление лишних строк

--- lab1/Form1.Designer.cs
+++ lab1/Form1.Designer.cs
@@ -8,15 +8,9 @@ namespace lab1
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
+      
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -28,10 +22,7 @@ namespace lab1
 
         #region Код, автоматически созданный конструктором форм Windows
 
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
+      
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
